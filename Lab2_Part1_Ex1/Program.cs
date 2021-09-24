5662d9f1e69dc26a582d85faffb3af7305c23acc
HoangNguyen <nguyen111@gmail.com>
2021-09-24 17:11:08 +0700

    addd

---
+++ Lab2_Part1_Ex1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab2_Part1_Ex1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var now = DateTime.Now;
+            var rand = new Random((int)now.Millisecond);
+            var arr = new int[10];
+            for (var x = 0; x < arr.Length; ++x)
+            {
+                arr[x] = rand.Next() % 100;
+            }
+            var total = 0;
+            Console.Write("Array values are");
+            foreach (var val in arr)
+            {
+                total += val;
+                Console.Write(val + ",");
+            }
+            Console.WriteLine("\nAnd the average is {0,0:F1}",
+            (double)total / (double)arr.Length);
+
+            Console.ReadLine();
+        }
+    }
+}
